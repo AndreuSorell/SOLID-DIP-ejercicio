b3c92c260be0842dc53c5ab8e3ffd8e2f61b42eb
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-04-11 00:35:30 +0200

    refactor(InventarioBBDD): Implementar la interfaz Inventario

--- src/main/java/edu/poniperro/bbdd/InventarioBBDD.java
+++ src/main/java/edu/poniperro/bbdd/InventarioBBDD.java
@@ -1,10 +1,10 @@
 package edu.poniperro.bbdd;
 
-public class InventarioBBDD {
+public class InventarioBBDD implements Inventario {
 
 
 	// Metodo de consulta a la base de datos
-	
+	@Override
 	public int numeroProductos(String tienda, String producto) {
 		return BBDD.stocs.get(tienda).get(producto);
 	}
