39321db07baf06451e1f91adc34e40e2afaabef2
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2022-04-11 00:39:46 +0200

    test: :white_check_mark: Adaptar los casos test

--- src/test/java/edu/poniperro/TestControladorEstoc.java
+++ src/test/java/edu/poniperro/TestControladorEstoc.java
@@ -3,14 +3,17 @@ package edu.poniperro;
 import org.junit.Assert;
 import org.junit.Test;
 
+import edu.poniperro.bbdd.Inventario;
 import edu.poniperro.bbdd.InventarioBBDD;
+import edu.poniperro.negocio.Controlador;
 import edu.poniperro.negocio.ControladorEstoc;
 
 public class TestControladorEstoc {
 
 	@Test
 	public void test_control_estoc() {
-		ControladorEstoc controlador = new ControladorEstoc(new InventarioBBDD());
+        Inventario inventario = new InventarioBBDD();
+		Controlador controlador = new ControladorEstoc(inventario);
 		
 		Assert.assertFalse(controlador.necesitaReponer("tienda norte", "mesa"));
 		Assert.assertTrue(controlador.necesitaReponer("tienda norte", "lampara"));
